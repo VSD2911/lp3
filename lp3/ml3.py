6ac416c09ed2c7da70a4cc43f4c605461a822810
Vaishnavi Dhinge <105734549+VSD2911@users.noreply.github.com>
2023-11-05 20:53:02 +0530

    Update ml3.py

--- lp3/ml3.py
+++ lp3/ml3.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import StandardScalers
+from sklearn.preprocessing import StandardScaler
 import io
 
 """## Read the Dataset"""
