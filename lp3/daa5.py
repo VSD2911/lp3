b17cb0e9366fc1e29a9b4d6bc61d0c9aaee14b8a
Vaishnavi Dhinge <105734549+VSD2911@users.noreply.github.com>
2023-11-08 02:20:57 +0530

    Update daa5.py

--- lp3/daa5.py
+++ lp3/daa5.py
@@ -1,45 +1,40 @@
-def is_safe(board, row, col):
+def is_safe(board,row,col):
     for i in range(row):
-        if board[i][col] == 1:
+        if board[i][col]==1:
             return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
+    for i,j in zip(range(row,-1,-1),range(col,-1,-1)):
+        if board[i][j]==1:
             return False
-    for i, j in zip(range(row, -1, -1), range(col, len(board))):
-        if board[i][j] == 1:
+    for i,j in zip(range(row,-1,-1),range(col,len(board))):
+        if board[i][j]==1:
             return False
     return True
 
-def solve_n_queens(board, row):
-    n = len(board)
-    if row == n:
+ 
+def solve_queen(board,row):
+    n=len(board)
+    if row==n:
         return True
-
     for col in range(n):
-        if is_safe(board, row, col):
-            board[row][col] = 1
-
-            if solve_n_queens(board, row + 1):
+        if is_safe(board,row,col):
+            board[row][col]=1
+            if solve_queen(board,row+1):
                 return True
-
-            board[row][col] = 0
-
+            board[row][col]=0
     return False
-
-def print_board(board):
+                    
+def printing(board):
     for row in board:
-        print(' '.join(['Q' if x == 1 else '.' for x in row]))
-
-n = 8
-chessboard = [[0] * n for _ in range(n)]
-
-
-chessboard[0][3] = 1
-if solve_n_queens(chessboard, 1):
-    print("Solution found:")
-    print_board(chessboard)
+        print(' '.join(['Q' if x==1 else '.' for x in row]))
+    
+n=int(input("No of queens"))
+board=[[0]*n for i in range(n)]
+board[0][3]=1
+if solve_queen(board,1):
+    print("sol")
+    printing(board)
 else:
-    print("No solution found.")
+    print("no sol")
 
 
 
